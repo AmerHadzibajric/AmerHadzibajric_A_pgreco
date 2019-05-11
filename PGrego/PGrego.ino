47c4043daf9242ec1c8a1d76feced765a5d1d527
Amer <amer.amer9211@gmail.com>
2019-05-11 08:44:23 +0200

    Variabili

--- PGrego/PGrego.ino
+++ PGrego/PGrego.ino
@@ -1,10 +1,16 @@
 int[] Arr = new int [4];
-
-
-
+int bottone1 = 2;
+int bottone2 = 7;
+int bottone3 = 8;
+int bottone4 = 11;
+int bottone5 = 13;
 void setup() {
   // put your setup code here, to run once:
-
+   pinMode(bottone1, INPUT);
+   pinMode(bottone2, INPUT);
+   pinMode(bottone3, INPUT);
+   pinMode(bottone4, INPUT);
+   pinMode(bottone5, INPUT);
 }
 
 void loop() {
