ec03f1444c1d8f3f6328c2116a02cb89ad935278
Amer <amer.amer9211@gmail.com>
2019-05-22 10:30:16 +0200

    Fine programma

--- PGrego/PGrego.ino
+++ PGrego/PGrego.ino
@@ -1,77 +1,103 @@
+#include <LiquidCrystal_I2C.h>
 #include <LiquidCrystal.h>
-LiquidCrystal lcd (14, 11, 9, 7, 3);
-int bottone1 = 2;
-int bottone2 = 7;
-int bottone3 = 8;
-int bottone4 = 11;
-int bottone5 = 13;
+//LiquidCrystal lcd (14, 11, 9, 7, 3, 1);
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+int bottone1 = 7;
+int bottone2 = 8;
+int bottone3 = 9;
+int bottone4 = 10;
+int bottone5 = 11;
+int btnInizio = 2;
 int vite = 5;
 int punti = 0;
 void setup() {
   // put your setup code here, to run once:
-  lcd.begin(16, 2);
-   pinMode(bottone1, INPUT);
-   pinMode(bottone2, INPUT);
-   pinMode(bottone3, INPUT);
-   pinMode(bottone4, INPUT);
-   pinMode(bottone5, INPUT);
+  //lcd.begin(16, 2);
+  lcd.init();
+  lcd.backlight();
+  pinMode(bottone1, INPUT);
+  pinMode(bottone2, INPUT);
+  pinMode(bottone3, INPUT);
+  pinMode(bottone4, INPUT);
+  pinMode(bottone5, INPUT);
+  pinMode(btnInizio, INPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-    int posizione =random(1,5);
-    if(posizione == 1)
+  while(digitalRead(btnInizio) == LOW){}
+  while ( vite > 0)
+  {
+    lcd.clear();
+    lcd.print("vite:" + String(vite));
+    lcd.setCursor(10, 0);
+    lcd.print("pt:" + String(punti));
+    delay(1500);
+    int posizione = random(1, 5);
+    if (posizione == 1)
     {
       posizione = 2;
-      
+
     }
-    else if(posizione == 2)
+    else if (posizione == 2)
     {
       posizione = 5;
     }
-    else if(posizione == 3)
+    else if (posizione == 3)
     {
       posizione = 8;
     }
-    else if(posizione == 4)
+    else if (posizione == 4)
     {
       posizione = 11;
     }
-    else if(posizione == 5)
+    else if (posizione == 5)
     {
       posizione = 14;
     }
 
-    lcd.setCursor(poszione, 1);
-    lcd.print("P");
-    int premuto =0;
+    lcd.setCursor(posizione, 1);
+    lcd.print("π");
+    int premuto = 0;
     int i = 0;
     while (premuto == 0 && i < 1500)
     {
-      if(digitalWrite(bottone1)==HIGH)
+      if (digitalRead(bottone1) == HIGH)
       {
         premuto = 2;
       }
-      else if(digitalWrite(bottone2)==HIGH)
+      else if (digitalRead(bottone2) == HIGH)
       {
-        premuto =5;
+        premuto = 5;
       }
-      else if(digitalWrite(bottone3)==HIGH)
+      else if (digitalRead(bottone3) == HIGH)
       {
-        premuto =8;
+        premuto = 8;
       }
-      else if(digitalWrite(bottone4)==HIGH)
+      else if (digitalRead(bottone4) == HIGH)
       {
-        premuto =11;
+        premuto = 11;
       }
-      else if(digitalWrite(bottone5)==HIGH)
+      else if (digitalRead(bottone5) == HIGH)
       {
-        premuto =14;
+        premuto = 14;
       }
       i++;
       delay(1);
     }
-  if(
-  
+    lcd.clear();
+    if (premuto == posizione)
+    {
+      punti = punti + 1;
+    }
+    else
+    {
+      vite = vite - 1;
+    }
 
+    lcd.print("vite:" + String(vite));
+    lcd.setCursor(10, 0);
+    lcd.print("pt:" + String(punti));
+  }
 }
